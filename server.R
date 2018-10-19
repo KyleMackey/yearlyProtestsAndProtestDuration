1b3997f74da615cdf15a8eed412df44e197e0569
Kyle Mackey <kylelmackey@gmail.com>
2018-10-19 11:27:15 -0400

    Update server.R

--- server.R
+++ server.R
@@ -6,7 +6,7 @@
 ##    File:     server.R                                        ##
 ##    Author:   Kyle Mackey                                     ##
 ##    Purpose:  Reactive plot of each country's yearly protests ##
-##              and yearly protest duration.                    ##                                      ##
+##              and yearly protest duration.                    ##                                      
 ##    Updated:  Oct 19, 2018                                    ##
 ##                                                              ##
 ##    Requires: protestnum.csv                                  ##
